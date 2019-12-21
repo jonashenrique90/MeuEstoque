cb3f09c4daf2237d820a429f7942d952369bad91
Jonas <jonas.herique90@gmail.com>
2019-12-21 19:55:47 -0300

    Atualiza menu para portugues

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
 
           <div class="card-body">
             <ul class="list-unstyled components">
-              <li class="active">
+              {{-- <li class="active">
                 <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Home</a>
                 <ul class="collapse list-unstyled" id="homeSubmenu">
                   <li>
@@ -22,11 +22,11 @@
                     <a href="#">Home 3</a>
                   </li>
                 </ul>
-              </li>
+              </li> --}}
               <li>
-                <a href="#">About</a>
+                <a href="#">Dashboard</a>
               </li>
-              <li>
+              {{-- <li>
                 <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Pages</a>
                 <ul class="collapse list-unstyled" id="pageSubmenu">
                   <li>
@@ -39,12 +39,9 @@
                     <a href="#">Page 3</a>
                   </li>
                 </ul>
-              </li>
+              </li> --}}
               <li>
-                <a href="#">Portfolio</a>
-              </li>
-              <li>
-                <a href="#">Contact</a>
+                <a href="#">Lista de Produtos</a>
               </li>
             </ul>
           </div>
